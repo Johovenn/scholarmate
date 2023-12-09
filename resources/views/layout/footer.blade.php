1c7a987e3f37f0f571be965cc278cf7ed50c6f5c
johovenn <110096623+Johovenn@users.noreply.github.com>
2023-12-09 08:53:26 +0700

    Modified Seeder

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -17,8 +17,8 @@
     <div class="d-flex flex-column">
         <p>Visit our social media : </p>
         <div class="d-flex">
-            <a class="text-light" target="_blank" href="https://www.instagram.com"><ion-icon class="me-3" size='large' name="logo-instagram"></ion-icon></a>
-            <a class="text-light" target="_blank" href="https://www.twitter.com"><ion-icon class="me-3" size='large' name="logo-twitter"></ion-icon></a>
+            <a class="text-light" target="_blank" href="https://www.instagram.com/myuda12"><ion-icon class="me-3" size='large' name="logo-instagram"></ion-icon></a>
+            <a class="text-light" target="_blank" href="https://www.twitter.com/"><ion-icon class="me-3" size='large' name="logo-twitter"></ion-icon></a>
             <a class="text-light" target="_blank" href="https://www.facebook.com"><ion-icon class="me-3" size='large' name="logo-facebook"></ion-icon></a>
         </div>
     </div>
